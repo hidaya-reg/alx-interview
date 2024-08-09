7559a3a9a417ca938c88b346c33c38b183f23867
hidaya <hidaya.regragui@gmail.com>
2024-08-09 21:38:26 +0100

    style

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,6 +3,7 @@
 0-pascal_triangle
 """
 
+
 def pascal_triangle(n):
     """
     Returns a list of lists of integers
@@ -10,20 +11,20 @@ def pascal_triangle(n):
     Returns an empty list if n <= 0.
     """
     triangle = []
-    
+
     if n <= 0:
         return triangle
-    
+
     triangle = [[1]]
-    
+
     for i in range(1, n):
         previous_row = triangle[i - 1]
         current_row = [1]
-        
+
         for j in range(len(previous_row) - 1):
             current_row.append(previous_row[j] + previous_row[j + 1])
-        
+
         current_row.append(1)
         triangle.append(current_row)
-    
+
     return triangle
