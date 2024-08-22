0de9d0e8d352904ccd550eb6d01aadad641d6345
hidaya <hidaya.regragui@gmail.com>
2024-08-22 22:16:17 +0100

    pycodestyle

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-""" 
+"""
     Minimum Operations
 """
-
 
 
 def minOperations(n: int) -> int:
@@ -12,14 +11,14 @@ def minOperations(n: int) -> int:
     """
     if n <= 1:
         return 0
-    
+
     operations = 0
     divisor = 2
-    
+
     while n > 1:
         while n % divisor == 0:
             operations += divisor
             n = n // divisor
         divisor += 1
-        
+
     return operations
