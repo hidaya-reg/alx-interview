c9d42ab10dbf4b5a1f52f99ba3a337c4d22f7426
hidaya <hidaya.regragui@gmail.com>
2024-11-14 10:27:03 +0100

    task0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,19 +2,27 @@
 
 const request = require('request');
 
-function fetchCharacters(characters, index) {
-  if (index >= characters.length) return;
-  request(characters[index], (err, res, body) => {
+function fetchCharacter(arr, index) {
+  if (index >= arr.length) return;
+
+  request(arr[index], (err, res, body) => {
     if (!err && res.statusCode === 200) {
       console.log(JSON.parse(body).name);
-      fetchCharacters(characters, index + 1);
+      fetchCharacter(arr, index + 1);
+    } else {
+      console.error('Error fetching character:', err);
     }
   });
 }
 
-request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, (err, res, body) => {
-  if (!err && res.statusCode === 200) {
-    const characters = JSON.parse(body).characters;
-    fetchCharacters(characters, 0);
+request(
+  `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`,
+  (err, res, body) => {
+    if (!err && res.statusCode === 200) {
+      const characters = JSON.parse(body).characters;
+      fetchCharacter(characters, 0); // Start fetching characters
+    } else {
+      console.error('Error fetching movie:', err);
+    }
   }
-});
+);
